6880837f44f9d25d27b910274566df316284dfae
Afshan-kh <159198384+Afshan-kh@users.noreply.github.com>
2024-02-22 02:23:10 +0500

    Add files via upload

---
+++ ques39.ts
@@ -0,0 +1,17 @@
+/*function city_country(city: string , country:string): string{
+  return `${city} , ${country}`;
+}
+
+console.log(city_country('"Karachi' , 'Pakistan"'));*/
+
+/*function city_country(city: string , country:string): string{
+  return `${city} , ${country}`;}
+
+
+console.log(city_country('"London' , 'United Kingdom"'));*/
+
+function city_country(city: string , country:string): string{
+  return `${city} , ${country}`;
+}
+
+console.log(city_country('"New York' , 'United States"'));
