306cc28bd3dd7f22fbad03f24c951f016535749b
Afshan-kh <159198384+Afshan-kh@users.noreply.github.com>
2024-03-01 02:23:10 +0500

    ques3.ts

--- ques3.ts
+++ ques3.ts
@@ -1,3 +1,17 @@
 let str = "afshan";
+
+
 console.log(str.toUpperCase());// for uppercase letters
+
+
 console.log(str.toLowerCase());//for lower case letters
+
+
+let Titlecase : string = "";
+ str.split(" ").forEach(word => {
+    const capitalWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    Titlecase += capitalWord + " ";
+
+});
+console.log(Titlecase);  //-------------- Titlecase
+
